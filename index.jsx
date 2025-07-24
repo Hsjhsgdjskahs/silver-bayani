f26a8326499a9a78185d3a0a755bb64f78b51858
Hsjhsgdjskahs <104725566+Hsjhsgdjskahs@users.noreply.github.com>
2025-07-24 20:41:14 +0330

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: 'center', paddingTop: '50px'}}>
+      <h1>به فروشگاه نقره Silver Bayani خوش آمدید ✨</h1>
+      <p>نسخه اولیه برای دیپلوی روی Vercel آماده است.</p>
+    </div>
+  );
+}
